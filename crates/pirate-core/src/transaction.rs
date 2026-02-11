320328289f6597234c217117938a16309d9e5d6d
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2026-02-11 17:49:07 +0200

    Switch back to internal change addresses

--- crates/pirate-core/src/transaction.rs
+++ crates/pirate-core/src/transaction.rs
@@ -368,7 +368,7 @@ impl TransactionBuilder {
         if change > 10_000 {
             // Dust threshold
             let change_addr = spending_key
-                .to_extended_fvk()
+                .to_internal_fvk()
                 .derive_address(change_diversifier_index)
                 .inner;
 
