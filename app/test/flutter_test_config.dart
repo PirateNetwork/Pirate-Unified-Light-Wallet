44a4152ab03ba727af37500a6b23c5ef13956c75
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2026-02-13 23:29:18 +0200

    Remove unused Golden tests

--- app/test/flutter_test_config.dart
+++ app/test/flutter_test_config.dart
@@ -1,11 +1,5 @@
 import 'dart:async';
-import '../test_driver/golden_test_config.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  // Configure golden test comparator
-  GoldenTestConfig.configure();
-
-  // Run tests
   await testMain();
 }
-
